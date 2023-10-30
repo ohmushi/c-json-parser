9ab753643c137c6374d58905b21002778b3fbe42
ohmushi <omnes.theo@gmail.com>
2023-10-30 11:17:01 +0100

    init lib

---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef C_JSON_PARSER_LIBRARY_H
+#define C_JSON_PARSER_LIBRARY_H
+
+void hello(void);
+
+#endif //C_JSON_PARSER_LIBRARY_H
---
+++ run.c
@@ -0,0 +1,13 @@
+
+//
+// Created by Theo OMNES on 30/10/2023.
+//
+
+#include "stdlib.h"
+#include "library.h"
+
+
+int main() {
+    hello();
+    return EXIT_SUCCESS;
+}
